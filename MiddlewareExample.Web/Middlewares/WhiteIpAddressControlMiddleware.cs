83c3c00956a9bf95aec25e8ab2bb51f335120126
canemre97 <canemre97@users.noreply.github.com>
2023-03-11 19:57:24 +0300

    Middleware

---
+++ MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace MiddlewareExample.Web.Middlewares
+{
+    public class WhiteIpAddressControlMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        private const string WhiteIpAddress = "::1";
+
+        public WhiteIpAddressControlMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //IPV4 => 127.0.0.1 => Localhost
+            //IPV6 => ::1 => localhost
+
+            var reqIpAddress = context.Connection.RemoteIpAddress;
+
+            bool AnyWhiteIpAdress = IPAddress.Parse(WhiteIpAddress).Equals(reqIpAddress);
+
+            if (AnyWhiteIpAdress)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode = HttpStatusCode.Forbidden.GetHashCode();
+                await context.Response.WriteAsync("Forbidden");
+            }
+
+        }
+    }
+}
